d45c5fa676573b6fb028135651c870e144e7f1f6
mariem <mariem@DESKTOP-IVK64VK>
2021-03-09 13:26:46 -0800

    tp1

---
+++ src/isetj/servlets/CoffeeServlet.java
@@ -0,0 +1,55 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CoffeeServlet
+ */
+@WebServlet("/CoffeeServlet")
+public class CoffeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CoffeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String c = request.getParameter("c");
+		String comment = request.getParameter("comment");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head>");
+		out.println("<title>Insert title here</title></head>");
+		out.println("<body><img src='coff.jpg'>");
+		out.println("<p>merci de nous fait parvenir la remarque suivante concernant </p>");
+		out.println("<b>"+c+"</b>");
+		out.println("<b>"+comment+"</b>");
+		out.println("</body></html>");
+				
+	
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
